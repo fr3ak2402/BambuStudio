070229d0b6fd5858477a8f3cb746e19815d068a5
fr3ak2402 <96239814+fr3ak2402@users.noreply.github.com>
2024-09-07 16:48:07 +0200

    Update build_win_vs2022.bat

--- build_win_vs2022.bat
+++ build_win_vs2022.bat
@@ -1,20 +1,12 @@
 @echo off
 
-:: Define sections
-:CompileDeps
-echo Compiling Dependencies
-:: Replace this with your actual compilation command for Deps
-
-:CompileSlicer
-echo Compiling Slicer
-:: Replace this with your actual compilation command for Slicer
-
 :: Main execution
 if "%~1"=="" goto :HELP
 if "%~1"=="-h" goto :HELP
 
 if "%~1"=="-A" (
     goto :CompileDeps
+:TriggerCompileSlicer
     goto :CompileSlicer
 )
 if "%~1"=="-D" goto :CompileDeps
@@ -34,5 +26,21 @@ echo   -A  Compile Dependencies and Slicer
 echo   -D  Compile Dependencies
 echo   -S  Compile Slicer
 echo ------------------------------
+
+goto :end
+
+:: Define sections
+:CompileDeps
+echo Compiling Dependencies
+:: Replace this with your actual compilation command for Deps
+
+if "%~1"=="-A" goto :TriggerCompileSlicer
+
+goto :end
+
+:CompileSlicer
+echo Compiling Slicer
+:: Replace this with your actual compilation command for Slicer
+goto :end
 
 :end
