928eec8174f20f28d4be0acefd85d905e4c14d46
fr3ak2402 <96239814+fr3ak2402@users.noreply.github.com>
2024-09-08 18:25:15 +0200

    Update build_slicer_win_vs2019.bat

--- build_slicer_win_vs2019.bat
+++ build_slicer_win_vs2019.bat
@@ -9,18 +9,20 @@ echo create folders for the compiling process
 
 :: create folders for the compiling process
 mkdir GalaxySlicerNeo
-cd GalaxySlicerNeo
-mkdir install_dir
+
+cd D:/BambuStudio
+
+:: create folders for the compiling process
 mkdir build
 cd build
 
 echo start compiling...
 
 :: compile slicer -> VS2019 and architecture x64
-cmake .. -G "Visual Studio 16 2019" -DBBL_RELEASE_TO_PUBLIC=1 -DCMAKE_PREFIX_PATH="D:/work/Projects/GalaxySlicerNeo_deps/usr/local" -DCMAKE_INSTALL_PREFIX="../install_dir" -DCMAKE_BUILD_TYPE=Release -DWIN10SDK_PATH="C:/Program Files (x86)/Windows Kits/10/Include/10.0.22621.0"
+cmake .. -G "Visual Studio 16 2019" -DBBL_RELEASE_TO_PUBLIC=1 -DCMAKE_PREFIX_PATH="D:/work/Projects/GalaxySlicerNeo_deps/usr/local" -DCMAKE_INSTALL_PREFIX="D:/work/Projects/GalaxySlicerNeo" -DCMAKE_BUILD_TYPE=Release -DWIN10SDK_PATH="C:/Program Files (x86)/Windows Kits/10/Include/10.0.22621.0"
 cmake --build . --target install --config Release
 
-cd D:/work/Projects/GalaxySlicerNeo/install_dir
+cd D:/work/Projects/GalaxySlicerNeo
 
 echo create folder for python
 
@@ -29,12 +31,12 @@ mkdir python
 echo Start deleting unnecessary files...
 
 :: delete all unnecessary data
-powershell -command "Remove-Item 'D:/work/Projects/GalaxySlicerNeo/build' -Recurse -Force"
+powershell -command "Remove-Item 'D:/BambuStudio/build' -Recurse -Force"
 
 echo Deletion of unnecessary files completed...
 
 echo copying Python...
 
-powershell -command "Copy-Item -Path D:/work/Projects/GalaxySlicerNeo_deps/python/* -Destination D:/work/Projects/GalaxySlicerNeo/install_dir/python -Recurse"
+powershell -command "Copy-Item -Path D:/work/Projects/GalaxySlicerNeo_deps/python/* -Destination D:/work/Projects/GalaxySlicerNeo/python -Recurse"
 
 PAUSE
